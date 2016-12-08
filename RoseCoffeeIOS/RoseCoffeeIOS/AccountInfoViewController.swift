5cc91b3bdf1b1588440ff6180af08f44d2620e3f
xxyicy <xiaox@rose-hulman.edu>
2016-12-08 17:59:56 -0500

    account info and menu screen

---
+++ RoseCoffeeIOS/RoseCoffeeIOS/AccountInfoViewController.swift
@@ -0,0 +1,57 @@
+//
+//  AccountInfoViewController.swift
+//  RoseCoffeeIOS
+//
+//  Created by Xinyu Xiao on 12/8/16.
+//  Copyright © 2016 Xinyu Xiao. All rights reserved.
+//
+
+import UIKit
+
+
+class AccountInfoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    @IBOutlet weak var profileImageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+         imagePicker.delegate = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func changeProfileImageBtnTapped(_ sender: UIButton) {
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // MARK: - UIImagePickerControllerDelegate Methods
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerEditedImage] as? UIImage {
+            profileImageView.contentMode = .scaleAspectFill
+            profileImageView.image = pickedImage
+            appDelegate.profileImage = pickedImage
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
---
+++ RoseCoffeeIOS/RoseCoffeeIOS/MenuController.swift
@@ -0,0 +1,31 @@
+//
+//  MenuController.swift
+//  RoseCoffeeIOS
+//
+//  Created by Xinyu Xiao on 12/8/16.
+//  Copyright © 2016 Xinyu Xiao. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UITableViewController {
+    @IBOutlet weak var profileImageView: UIImageView!
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let image = appDelegate.profileImage {
+            profileImageView.contentMode = .scaleAspectFill
+            profileImageView.image = image
+        }
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
