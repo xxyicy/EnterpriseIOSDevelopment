17916a2a8c6074b8fec3358909a1e58f77d88ea4
MainTankJerry <qiuz@rose-hulman.edu>
2016-12-13 14:06:06 -0500

    Keyboard disappear when click on background

--- RoseCoffeeIOS/RoseCoffeeIOS/ViewController.swift
+++ RoseCoffeeIOS/RoseCoffeeIOS/ViewController.swift
@@ -13,11 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func dismissKeyboard(){
+        view.endEditing(true)
     }
 
 
