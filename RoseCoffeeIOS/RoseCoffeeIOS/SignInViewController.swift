6a275ddaaf6cc4ff28276d78b438b05af8b85560
Fangyuan Wang, 王方圆 <wangfangyuan1995@gmail.com>
2016-12-31 15:05:37 -0500

    added sign in view controller

---
+++ RoseCoffeeIOS/RoseCoffeeIOS/SignInViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SignInViewController.swift
+//  RoseCoffeeIOS
+//
+//  Created by FengYizhi on 2016/12/31.
+//  Copyright © 2016年 Xinyu Xiao. All rights reserved.
+//
+
+import Foundation
+
+class SignInViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    @IBAction func SignInAction(_ sender: UIButton) {
+        if usernameTextField.text != "" {
+            if passwordTextField.text == "1234" {
+                let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                let navController: UINavigationController = storyboard.instantiateViewController(withIdentifier: "AccountInfoNav") as! UINavigationController
+                
+                self.revealViewController().pushFrontViewController(navController, animated: true)
+                
+            }
+        }
+    }
+}
