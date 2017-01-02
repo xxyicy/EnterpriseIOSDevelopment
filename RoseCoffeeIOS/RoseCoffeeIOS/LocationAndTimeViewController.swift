566872e760ae83841e5675a3e33794fee01ba055
MainTankJerry <qiuz@rose-hulman.edu>
2017-01-02 15:39:17 -0500

    Add Order detail page. Just a minor commit. Jerry

--- RoseCoffeeIOS/RoseCoffeeIOS/LocationAndTimeViewController.swift
+++ RoseCoffeeIOS/RoseCoffeeIOS/LocationAndTimeViewController.swift
@@ -8,6 +8,27 @@
 
 import UIKit
 
+//struct Player {
+//    var name: String?
+//    var game: String?
+//    var rating: Int
+//    
+//    init(name: String?, game: String?, rating: Int) {
+//        self.name = name
+//        self.game = game
+//        self.rating = rating
+//    }
+//}
+//
+//let playersData = [
+//    Player(name:"Bill Evans", game:"Tic-Tac-Toe", rating: 4),
+//    Player(name: "Oscar Peterson", game: "Spin the Bottle", rating: 5),
+//    Player(name: "Dave Brubeck", game: "Texas Hold 'em Poker", rating: 2) ]
+
 class LocationAndTimeViewController : UIViewController {
     
+    
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return transportItems.count
+//    }
 }
